f912ed204f232b71836f728842c18fdbfac630e0
Moses613 <44594121+Moses613@users.noreply.github.com>
2019-02-14 21:25:49 -0500

    java hwk chap 4 take two

--- src/PenniesForPay.java
+++ src/PenniesForPay.java
@@ -1,7 +1,28 @@
-public class Main {
+import java.util.Scanner;
+/** Chapter 4 Programming Challenge #4
+ * @author Moses613
+ */
+public class PenniesForPay {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("THIS IS GITCAMP");
+
+        Scanner keyboard = new Scanner(System.in);
+        double totalPay;
+        int daysWorked;
+
+        System.out.println("Please type the number of days worked, followed by the enter key.");
+        daysWorked = keyboard.nextInt();
+
+        //input validation
+        while (daysWorked < 1) {
+            System.out.println("\nThe number of days must be greater than zero. Try again.");
+            daysWorked = keyboard.nextInt();
+        }
+
+        //Print results
+        for (int i = 1; i <= daysWorked; i++) {
+            totalPay = Math.pow(2, (i - 1));
+            System.out.println("Day " + i + ": Pay is " + totalPay);
+        }
     }
 }
