23a2009c685d651d8c9b7de33fee3a884dbf81d1
Moses613 <44594121+Moses613@users.noreply.github.com>
2019-02-27 00:32:08 -0500

    finished hwk 5: Dice Game

--- src/Die.java
+++ src/Die.java
@@ -1,6 +1,51 @@
+import java.util.Random;
+
 /** Homework #5 - Dice Game Die class
+ * Simulates a multi-sided die
  * @author Moses613
  */
 public class Die {
 
+    private int sides;
+    private int currentValue;
+
+    /**
+     * No-arg constructor assumes the die is six-sided, and performs an initial roll
+     */
+    public Die () {
+        sides = 6;
+        roll();
+    }
+
+    /** Regular constructor, takes input for the number of sides the die should have, also performs an intial roll
+     * @param numSides The number of sides the die should have
+     */
+    public Die (int numSides) {
+        sides = numSides;
+        roll();
+    }
+
+    /**
+     * Rolls the die
+     */
+    public void roll() {
+        Random numeroGenerator = new Random();
+        currentValue = numeroGenerator.nextInt(sides) + 1;
+    }
+
+    /** Returns the current value of the die
+     *
+     * @return Returns the current value of the die
+     */
+    public int getCurrentValue() {
+        return currentValue;
+    }
+
+    /**Returns the number of sides the die was set to
+     *
+     * @return Returns the number of sides the die was set to
+     */
+    public int getSides() {
+        return sides;
+    }
 }
--- src/Homework5.java
+++ src/Homework5.java
@@ -1,10 +1,66 @@
+import java.util.Scanner;
+
 /** Homework #5 - Dice Game main class
  * @author Moses613
  */
 public class Homework5 {
 
     public static void main(String[] args) {
+        //Creates the dice
+        Die computerDie = new Die(6);
+        Die userDie = new Die(6);
+
+        int userScore = 0; //Tracks the total matches won by the user
+        int computerScore = 0; //Tracks the total matches won by the computer
+
+        Scanner input = new Scanner(System.in); //So that the user can progress the game by pushing enter
+
+        System.out.println("Now starting the ten-round Dice Game(TM)...\n\n");
+        //runs ten rounds of the dice game
+        for (int i = 0; i < 10; i++ ) {
+            //Roll dice
+            computerDie.roll();
+            System.out.println("The computer rolled: " + computerDie.getCurrentValue() + "\n");
+            System.out.print("It's your turn! Press enter to roll your die.\n");
+            input.nextLine(); //The user pushes enter to initiate die rolling
+            userDie.roll();
+            System.out.println("The user rolled: " + userDie.getCurrentValue());
+
+            //Compute results
+            if (computerDie.getCurrentValue() > userDie.getCurrentValue()){
+                System.out.println("The computer has won the match.");
+                computerScore++;
+            }
+            else if (userDie.getCurrentValue() > computerDie.getCurrentValue()) {
+                System.out.println("The user has won the match.");
+                userScore++;
+            }
+            else { //For a tie
+                System.out.println("The match ended in a tie.");
+            }
+
+            //Display round progression message assuming the ten rounds have not yet been completed
+            if (i != 9) {
+                System.out.println("\nPress enter to progress to Round " + (i + 2));
+                input.nextLine();
+            }
+
+        }
+
+        //Display final results
+        System.out.println("\n\n***Final Results*** \n");
+        System.out.println("The user won " + userScore + " matches.");
+        System.out.println("The computer won " + computerScore + " matches.");
+
+        if (userScore > computerScore) {
+            System.out.println("\nCongratulations! You've won the Dice Game(TM)!!");
+        }
+        else {
+            System.out.println("\nYou would appear to have lost... Better luck next time.");
+        }
+
 
     }
+
 
 }
