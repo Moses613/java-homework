e4c5f23381d31af2589dde2781370f346b4c13a1
Moses613 <44594121+Moses613@users.noreply.github.com>
2019-03-18 17:38:22 -0400

    Finished hwk 6 yay

--- src/Homework6.java
+++ src/Homework6.java
@@ -8,14 +8,16 @@ public class Homework6 {
     public static void main(String[] args) throws IOException {
         int timesWon = 0;
         String[] array = createStringArray("src/WorldSeriesWinnersHwk6.txt");
-
         Scanner input = new Scanner(System.in);
-        System.out.println("This program display the number of times an inputted team has won" +
-                " the World Series.\n");
-        System.out.println("Please enter the name of the team you wish to search for:");
-        String team = input.nextLine();
 
+        System.out.println("This program displays the number of times an inputted team has won" +
+                " the World Series.\n");
+        System.out.println("Please enter the name of the team you wish to search for.");
+        System.out.println("Do not include the word \"the\".");
+        String team = input.nextLine();
         timesWon = search(array, team);
+
+        System.out.println("The team \"" + team + "\" has won " + timesWon + " times.");
 
     }
 
@@ -49,6 +51,12 @@ public class Homework6 {
 
     public static int search(String[] array, String team) {
         int timesWon = 0;
+
+        for (int counter = 0; counter < array.length; counter++) {
+            if (team.equalsIgnoreCase(array[counter])) {
+                timesWon++;
+            }
+        }
 
         return timesWon;
     }
